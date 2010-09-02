7ce621e84b2f0686dfe49ce7507930e4085db1d5
Andy Nicholson <andy@infiniterecursion.com.au>
2010-09-03 02:24:46 +1000

    messaging work

---
+++ meow-iphone/MEOW-iphone/Classes/MEOW_UserMessage.h
@@ -0,0 +1,33 @@
+//
+//  MEOW_UserMessage.h
+//  MEOW-iphone
+//
+//  Created by andycat on 2/09/10.
+//  Copyright 2010 Infinite Recursion Pty Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define MSG_PRIVATE 0
+#define MSG_GROUP 1
+#define MSG_PUBLIC 2
+
+@interface MEOW_UserMessage : NSObject {
+
+	NSString *sender;
+	NSString *datetime;
+	NSString *readat_datetime;
+	NSString *message;
+	NSString *title;
+	NSString *type;
+}
+
+@property (nonatomic, retain) NSString *sender;
+@property (nonatomic, retain) NSString *datetime;
+@property (nonatomic, retain) NSString *message;
+@property (nonatomic, retain) NSString *title;
+@property (nonatomic, retain) NSString *type;
+@property (nonatomic, retain) NSString *readat_datetime;
+
+
+@end
---
+++ meow-iphone/MEOW-iphone/Classes/MEOW_UserMessage.m
@@ -0,0 +1,16 @@
+//
+//  MEOW_UserMessage.m
+//  MEOW-iphone
+//
+//  Created by andycat on 2/09/10.
+//  Copyright 2010 Infinite Recursion Pty Ltd. All rights reserved.
+//
+
+#import "MEOW_UserMessage.h"
+
+
+@implementation MEOW_UserMessage
+
+@synthesize sender, datetime, message, title, type, readat_datetime;
+
+@end
